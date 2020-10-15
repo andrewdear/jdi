b0233df1af03f356767a9ff956ad43da54f898cc
Andrew Dear <andrewdear2@gmail.com>
2020-10-15 22:09:08 +0100

    added custom methods logic

--- src/PathBuilder.php
+++ src/PathBuilder.php
@@ -1,67 +1,105 @@
 <?php
+
 namespace JDI\Helper;
 
 class PathBuilder
 {
-  /**
-   * Concatenate a path with a custom separator
-   *
-   * @param string   $separator
-   * @param string[] $pathComponents
-   *
-   * @return string
-   */
-  public static function custom($separator, array $pathComponents)
-  {
-    //TODO: Return path as string
-  }
+    /**
+     * Concatenate a path with a custom separator
+     *
+     * @param string $separator
+     * @param string[] $pathComponents
+     *
+     * @return string
+     */
+    public static function custom($separator, array $pathComponents)
+    {
+        // remove all empty strings and nulls so we have clean data, has to be done so i can look at the first and last bit of actual data to look for trailing separators.
+        $pathComponents = array_values(array_filter($pathComponents, function ($item) {
+            return $item !== null && $item !== "";
+        }));
 
-  /**
-   * Concatenate any number of path sections and correctly
-   * handle directory separators
-   *
-   * @param array $parts
-   *
-   * @return string
-   */
-  public static function system(...$parts)
-  {
-    return static::custom(DIRECTORY_SEPARATOR, $parts);
-  }
+        $arrayLength = sizeof($pathComponents);
 
-  /**
-   * Concatenate a path with windows style path separators
-   *
-   * @param array $parts
-   *
-   * @return string
-   */
-  public static function windows(...$parts)
-  {
-    return static::custom('\\', $parts);
-  }
+        for ($i = 0; $i < $arrayLength; $i++) {
+            $part = $pathComponents[$i];
 
-  /**
-   * Concatenate a path with unix style path separators
-   *
-   * @param array $parts
-   *
-   * @return string
-   */
-  public static function unix(...$parts)
-  {
-    return static::custom('/', $parts);
-  }
+            if (is_numeric($part)) {
+                $part = (string)$part;
+            }
 
-  /**
-   * Concatenate a path with unix style path separators
-   *
-   * @param array $parts
-   *
-   * @return string
-   */
-  public static function url(...$parts)
-  {
-    return static::custom('/', $parts);
-  }
+            // check if any but the first has a leading separator
+            if ($i != 0) {
+                $firstChar = $part[0];
+
+                if ($firstChar == $separator) {
+                    $part = substr($part, 1, strlen($part) - 1);
+                }
+            }
+
+            // check if any but the last has a trailing separator
+            if ($i != $arrayLength - 1) {
+                $lastChar = $part[strlen($part) - 1];
+
+                if ($lastChar == $separator) {
+                    $part = substr($part, 0, strlen($part) - 1);
+                }
+            }
+
+            $pathComponents[$i] = $part;
+        }
+
+
+
+        return join($separator ,$pathComponents);
+    }
+
+    /**
+     * Concatenate any number of path sections and correctly
+     * handle directory separators
+     *
+     * @param array $parts
+     *
+     * @return string
+     */
+    public static function system(...$parts)
+    {
+        return static::custom(DIRECTORY_SEPARATOR, $parts);
+    }
+
+    /**
+     * Concatenate a path with windows style path separators
+     *
+     * @param array $parts
+     *
+     * @return string
+     */
+    public static function windows(...$parts)
+    {
+        return static::custom('\\', $parts);
+    }
+
+    /**
+     * Concatenate a path with unix style path separators
+     *
+     * @param array $parts
+     *
+     * @return string
+     */
+    public static function unix(...$parts)
+    {
+        return static::custom('/', $parts);
+    }
+
+    /**
+     * Concatenate a path with unix style path separators
+     *
+     * @param array $parts
+     *
+     * @return string
+     */
+    public static function url(...$parts)
+    {
+        return static::custom('/', $parts);
+    }
 }
